67ec1a6700de32e637179ef7cd7e896c7a0cd81d
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-27 03:43:48 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,28 +3,62 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
+        :name =>  "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
         #your key/value pairs here
+        :name =>  "Lady Montague",
+        :age => "54"
         },
       :hero => {
+        :name =>  "Romeo",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
-      :hero_friends => []
-   }, 
+      :hero_friends => [
+        {
+          :name => "Benevoli",
+          :age => "17",
+          :attitude => "worried"
+        },
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
+      ]
+   },
    :capulet => {
       :patriarch => {
+        :name =>  "Lord Capulet",
+        :age => "50"
         #your key/value pairs here
         },
       :matriarch => {
+        :name =>  "Lady Capulet",
+        :age => "51"
         #your key/value pairs here
         },
       :heroine => {
         #your key/value pairs here
+        :name =>  "Juliet",
+        :age => "15",
+        :status => "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
+      ]
    }
   }
-
-  
 end
